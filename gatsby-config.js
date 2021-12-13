9b3cb7daa8f37cf5f7a6a69df45788d3f0300b05
Vinay Nair <vnnair39@gmail.com>
2021-12-13 13:36:22 +0530

    Updated footer locations and keywords

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,48 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env`,
 });
 module.exports = {
   siteMetadata: {
-    title: "Safeline Electricals",
-    titleTemplate: "Safeline Electricals",
+    title: 'Safeline Electricals',
+    titleTemplate: 'Safeline Electricals',
     description:
-      "A Grade Government Registered & Licensed Electrical Contractors, Engineers & Consultants. Safeline is one of the most trusted contractors from Mumbai, Maharashtra.",
-    url: "https://safelineelectricals.in", // No trailing slash allowed!
-    siteUrl: "https://safelineelectricals.in",
-    locale: "en-IN",
-    image: "/logo.png",
+      'A Grade Government Registered & Licensed Electrical Contractors, Engineers & Consultants. Safeline is one of the most trusted contractors from Mumbai, Maharashtra.',
+    url: 'https://safelineelectricals.in', // No trailing slash allowed!
+    siteUrl: 'https://safelineelectricals.in',
+    locale: 'en-IN',
+    image: '/logo.png',
     keywords:
-      "Electrical Contractors, Electrical Engineers, Electrical Consultants, Top commercial electrical contractors, Residential Electrical, Industrial Electrical Project services, Office Electrical Contractor Services, Commercial Electrical Contractor For IT Park, Contractor Services For Building, Govt. Licensed Electrical Contractor, Electrician business, 5 star electric, Electrical installation, Industrial Electrician, Local Electrical Contractor, Electrical Company, Electrical contractors in navi mumbai, electrical contractors in mumbai, electrical contractor website, electrical contract jobs, top electrical contractor, electrical contractor work, Electrical contractor in pune, contractor, contractor near me, contractor mumbai, contractor pune, contractor taloja, contractor maharashtra, Licensed Electrical Contractor, Govt Licensed Electrical Contractor, Government Registered Contractor, Switchgear, Best Electrical Contractor, Most Rated, Budget, Navi Mumbai, Switchgears & Spares, Transformers, Distribution Transformers, Voltage Switchgears, Lightning Arrestors, Pre Fabricated Substation, Compact Switchgears, Ht Switchgears, Lt Switchgears, Lt Panels, Internal Electrical Distribution Works, New Substation Works, Retrofitting Work, Power Cabling, Earthing Works, Annual Maintenance Contract, Preventive Maintenance, Maintenance Work, Testing Works, Electrical Works For DG Sets, Authorized Dealer For Areva Switchgear, Authorized Dealer For Schneider, H.T.Switchgears, L.T.Switchgears, L.T. Panels, Protection Systems, Power Factor Improvement, Electrical Audit, Battery Chargers, Overhead Line Works, General Facilities Drilling, Jigsaw, Cutting, Bending, Welding, Crimping, Tools, Tackles, india, indian, mumbai, navi Mumbai, industrial, industries, thane, pune, nashik, aurangabad, ratnagiri, nagpur, ahmednagar, akola, amravati, chandrapur, dhule, jalgaon, raigad, sangli, satara, belgaum, kolhapur, belgaon, Taloja, panvel, kamothane, maharashtra",
+      'Electrical Contractors, Licensed Electrical Contractor, Switchgear, Switchgears & Spares, Transformers, Distribution Transformers, Voltage Switchgears, Lightning Arrestors, Pre Fabricated Substation, Compact Switchgears, Ht Switchgears, Lt Switchgears, Lt Panels, Internal Electrical Distribution Works, Substation Works, Retrofitting Work, Power Cabling, Earthing Works, Annual Maintenance Contract, Preventive Maintenance, Maintenance Work, Testing Works, Electrical Works For DG Sets, , HT Switchgears, LT Switchgears, L.T. Panels, Protection Systems, Power Factor Improvement, Electrical Audit, Battery Chargers, Overhead Line Works, General Facilities Drilling, Jigsaw, Cutting, Bending, Welding, Crimping, Tools, Tackles , >Licensed Electrical Contractor, HT / LT Switchgears, LT Panels, Distribution Transformers, Mumbai, India Civil contractors, fabrication contractors, pipe line and piping ,tank ,civil, civil work, fabrication, structural work, road concrete construction, drainage and plumbing work, pavar block installation',
   },
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
         spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /assets/, // See below to configure properly
         },
       },
-    },    
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    "gatsby-transformer-sharp",
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
       resolve: `gatsby-plugin-manifest`,
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "gatsby";
-import logo from "../../images/logo.png";
+import React from 'react';
+import { Link } from 'gatsby';
+import logo from '../../images/logo.png';
 
-import styles from "./Footer.module.scss";
+import styles from './Footer.module.scss';
 
 interface Props {}
 const Footer: React.FC<Props> = ({}) => {
@@ -14,9 +14,12 @@ const Footer: React.FC<Props> = ({}) => {
           <p>Safeline Electricals</p>
         </Link>
         <div className={styles.title}>
-          A Grade Government Registered & Licensed Electrical Contractor. Safeline Also Undertakes all types of civil and mechanical works.
+          A Grade Government Registered & Licensed Electrical Contractor.
+          Safeline Also Undertakes all types of civil and mechanical works.
         </div>
-        <div className={styles.copyright}>Copyright © 2021 by Safeline Electricals</div>
+        <div className={styles.copyright}>
+          Copyright © 2021 by Safeline Electricals
+        </div>
       </div>
       <div className={styles.right}>
         <div className={styles.main}>
@@ -29,24 +32,36 @@ const Footer: React.FC<Props> = ({}) => {
             <Link to="/careers">Careers</Link>
             <Link to="/contacts">Contact Us</Link>
             <Link to="/blogs">Blogs</Link>
-
           </div>
           <div className={styles.details}>
             <p>Contact Us</p>
             <div className={styles.address}>SAFELINE ELECTRICALS</div>
-            <div className={styles.address} style={{ paddingTop: "0" }}>
-              Plot No.ISP -10, Opp.Sanghi Orgn., Near Petrol Pump, MIDC Taloja Industrial Area, Navi Mumbai - 410208 Maharashtra, India
+            <div className={styles.address} style={{ paddingTop: '0' }}>
+              Plot No.ISP -10, Opp.Sanghi Orgn., Near Petrol Pump, MIDC Taloja
+              Industrial Area, Navi Mumbai - 410208 Maharashtra, India
             </div>
-            <div className={styles.phone}>Phone : (022) 27401956 / 8425846364 </div>
-            <div className={styles.email}> Email: safelineelectricals@gmail.com</div>
+            <div className={styles.phone}>
+              Phone : (022) 27401956 / 8425846364{' '}
+            </div>
+            <div className={styles.email}>
+              {' '}
+              Email: safelineelectricals@gmail.com
+            </div>
             <div className={styles.places}>
-              Mumbai, Navi Mumbai, industrial, industries, thane, pune, nashik, aurangabad, ratnagiri, nagpur, ahmednagar, akola, amravati, chandrapur, dhule,
-              jalgaon, raigad, sangli, satara, belgaum, kolhapur, belgaon, Taloja, panvel, kamothe, maharashtra.
+              Mumbai, Maharashtra, Industrial, Industries, Thane, Taloja MIDC,
+              MIDC, TTC, Tarapur, Dombivali, Airoli, Ghansoli, Mahape,
+              Koparkherane, Beleur, Kharghar, Ambernath, Turbhe, Vashi, Nerul,
+              Rabale, Karjat, Alibag, Raigad, Mumbai, Navi Mumbai, Panvel, Uran,
+              Pune, Nashik, Aurangabad, Ratnagiri, Nagpur, Ahmednagar, Akola,
+              Amravati, Chandrapur, Dhule, Jalgaon, Raigad, Ratnagiri, Sangli,
+              Satara, Belgaum, Kolhapur, Belgaon
             </div>
           </div>
         </div>
       </div>
-      <div className={styles.mobileCopyright}>Copyright © 2021 by Safeline Electricals</div>
+      <div className={styles.mobileCopyright}>
+        Copyright © 2021 by Safeline Electricals
+      </div>
     </div>
   );
 };
