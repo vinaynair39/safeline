a0a61cdd0209f6eceea31a213e40a05837788400
Vinay Nair <vnnair39@gmail.com>
2022-01-30 22:37:50 +0530

    feat: Change welcome info and navbar button text change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
-import { graphql, Link, useStaticQuery } from "gatsby";
-import classnames from "classnames";
-import React from "react";
-import styles from "./Navbar.module.scss";
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import classnames from 'classnames';
+import React from 'react';
+import styles from './Navbar.module.scss';
 
 interface Props {
   path: string;
@@ -34,12 +34,22 @@ const Navbar: React.FC<Props> = ({ path }) => {
     <nav className={styles.navbar}>
       <div className={styles.dropdown}>
         <div className={styles.menuItem}>
-          <Link to="/" className={classnames(styles.button, { [styles.active]: path === "/" })}>
+          <Link
+            to="/"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/',
+            })}
+          >
             Home
           </Link>
         </div>
         <div className={styles.menuItem}>
-          <a href="#welcomeInfo" className={classnames(styles.button, { [styles.active]: path === "/about" })}>
+          <a
+            href="#welcomeInfo"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/about',
+            })}
+          >
             About Us
           </a>
           <ul className={styles.menu}>
@@ -58,71 +68,129 @@ const Navbar: React.FC<Props> = ({ path }) => {
           </ul>
         </div>
         <div className={styles.menuItem}>
-          <a href="#services" className={classnames(styles.button, { [styles.active]: path === "/services" })}>
+          <a
+            href="#services"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/services',
+            })}
+          >
             Services
           </a>
           <ul className={styles.menu}>
             <li>
               <button>Electrical Services</button>
               <ul className={styles.submenu}>
-                {data.ELECTRICAL.nodes.map(({ slug, serviceName }: { slug: string; serviceName: string }) => {
-                  return (
-                    <Link to={`/${slug}`} className={styles.submenuLink} key={slug}>
-                      <li>{serviceName}</li>
-                    </Link>
-                  );
-                })}
+                {data.ELECTRICAL.nodes.map(
+                  ({
+                    slug,
+                    serviceName,
+                  }: {
+                    slug: string;
+                    serviceName: string;
+                  }) => {
+                    return (
+                      <Link
+                        to={`/${slug}`}
+                        className={styles.submenuLink}
+                        key={slug}
+                      >
+                        <li>{serviceName}</li>
+                      </Link>
+                    );
+                  }
+                )}
               </ul>
             </li>
             <li>
               <button>Mechanical Services</button>
               <ul className={styles.submenu}>
-                {data.MECHANICAL.nodes.map(({ slug, serviceName }: { slug: string; serviceName: string }) => {
-                  return (
-                    <Link to={`/${slug}`} key={slug}>
-                      <li>{serviceName}</li>
-                    </Link>
-                  );
-                })}
+                {data.MECHANICAL.nodes.map(
+                  ({
+                    slug,
+                    serviceName,
+                  }: {
+                    slug: string;
+                    serviceName: string;
+                  }) => {
+                    return (
+                      <Link to={`/${slug}`} key={slug}>
+                        <li>{serviceName}</li>
+                      </Link>
+                    );
+                  }
+                )}
               </ul>
             </li>
             <li>
-              <button>Other Services</button>
+              <button>Civil Services</button>
               <ul className={styles.submenu}>
-                {data.OTHERS.nodes.map(({ slug, serviceName }: { slug: string; serviceName: string }) => {
-                  return (
-                    <Link to={`/${slug}`} key={slug}>
-                      <li>{serviceName}</li>
-                    </Link>
-                  );
-                })}
+                {data.OTHERS.nodes.map(
+                  ({
+                    slug,
+                    serviceName,
+                  }: {
+                    slug: string;
+                    serviceName: string;
+                  }) => {
+                    return (
+                      <Link to={`/${slug}`} key={slug}>
+                        <li>{serviceName}</li>
+                      </Link>
+                    );
+                  }
+                )}
               </ul>
             </li>
           </ul>
         </div>
         <div className={styles.menuItem}>
-          <Link to="/projects" className={classnames(styles.button, { [styles.active]: path === "/projects" })}>
+          <Link
+            to="/projects"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/projects',
+            })}
+          >
             Projects
           </Link>
         </div>
         <div className={styles.menuItem}>
-          <Link to="/clients" className={classnames(styles.button, { [styles.active]: path === "/clients" })}>
+          <Link
+            to="/clients"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/clients',
+            })}
+          >
             Our clients
           </Link>
         </div>
         <div className={styles.menuItem}>
-          <Link to="/careers" className={classnames(styles.button, { [styles.active]: path === "/careers" })}>
+          <Link
+            to="/careers"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/careers',
+            })}
+          >
             Careers
           </Link>
         </div>
         <div className={styles.menuItem}>
-          <Link to="/contacts" className={classnames(styles.button, { [styles.active]: path === "/contacts" })}>
+          <Link
+            to="/contacts"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/contacts',
+            })}
+          >
             Contact Us
           </Link>
         </div>
         <div className={styles.menuItem}>
-          <Link to="/blogs" className={classnames(styles.button, { [styles.active]: path === "/blogs" })}>
-           Blogs
+          <Link
+            to="/blogs"
+            className={classnames(styles.button, {
+              [styles.active]: path === '/blogs',
+            })}
+          >
+            Blogs
           </Link>
         </div>
       </div>
--- src/components/WelcomeInfo/WelcomeInfo.tsx
+++ src/components/WelcomeInfo/WelcomeInfo.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import electric from "../../images/electric.webp";
-import tower from "../../images/tower.webp";
-import { motion } from "framer-motion";
-import SlipInWhenVisible from "../SlipInWhenVisible/SlipInWhenVisible";
-import Satisfaction from "../../assets/satisfaction.svg";
-import Badge from "../../assets/badge.svg";
+import React from 'react';
+import electric from '../../images/electric.webp';
+import tower from '../../images/tower.webp';
+import { motion } from 'framer-motion';
+import SlipInWhenVisible from '../SlipInWhenVisible/SlipInWhenVisible';
+import Satisfaction from '../../assets/satisfaction.svg';
+import Badge from '../../assets/badge.svg';
 
-import styles from "./WelcomeInfo.module.scss";
+import styles from './WelcomeInfo.module.scss';
 
 interface Props {}
 const WelcomeInfo: React.FC<Props> = ({}) => {
@@ -26,20 +26,28 @@ const WelcomeInfo: React.FC<Props> = ({}) => {
 
         <div className={styles.content}>
           <p className={styles.pretitle}>About Safeline</p>
-          <p className={styles.title}>We Are Authorised Zonal Contractor Of Indian Oil For All Type Of Electrical, Civil & Mechanical Work.</p>
-          <p className={styles.description}>
-            Founded by Mr. Harish Madhavi in 1997 and thereafter driven to successful, award-winning, and most trusted “A” class Electrical Contractors with
-            determination of its tag line – "To go an extra mile" after completion of every project.
+          <p className={styles.title}>
+            SAFELINE ELECTRICALS is one of the leading player in providing
+            Services and projects across India
           </p>
           <p className={styles.description}>
-            Today Safeline is synonymous with Technical & Managerial expertise and is on of the most trusted contractors from Mumbai, Maharashtra. Safeline has
-            financial fortitude for the victorious accomplishment of various Projects.
+            The Company is engaged in servies like Electrical, Mechanical, Civil
+            for more than 25 years and has worked in over various states in
+            india, and also has executed prestigious Projects for Center, State
+            Governments as well as Private Sectors.
+          </p>
+          <p className={styles.description}>
+            The Company is also engaged in projects with enormous corporations
+            like Indian Oil Corporation Limited, Maharashtra Industrial
+            Development Corportation, Mahanagar Gas Ltd, Lubrizol India Private
+            Limited, Steel Authority of India. We are authorised empanelled
+            vendor for this scheme.
           </p>
 
           <motion.div className={styles.usp}>
             <motion.div className={styles.badge}>
               <Badge />
-              <p>20+ years of Professional Experiance</p>
+              <p>25+ years of Professional Experiance</p>
             </motion.div>
             <motion.div className={styles.satisfaction}>
               <Satisfaction />
