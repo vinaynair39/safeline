4addb6e46ab998fb5966d22acca9ec0ebd9c15f2
Vinay Nair <vnnair39@gmail.com>
2022-07-31 14:53:55 +0530

    career text changes

--- src/components/QuoteForm/QuoteForm.tsx
+++ src/components/QuoteForm/QuoteForm.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import { ErrorMessage } from "@hookform/error-message";
-import Arrow from "../../assets/arrow.svg";
-import Telephone from "../../assets/telephone.svg";
-import classNames from "classnames";
-import Modal from "../Modal/Modal";
-import SlipInWhenVisible from "../SlipInWhenVisible/SlipInWhenVisible";
-import styles from "./QuoteForm.module.scss";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { ErrorMessage } from '@hookform/error-message';
+import Arrow from '../../assets/arrow.svg';
+import Telephone from '../../assets/telephone.svg';
+import classNames from 'classnames';
+import Modal from '../Modal/Modal';
+import SlipInWhenVisible from '../SlipInWhenVisible/SlipInWhenVisible';
+import styles from './QuoteForm.module.scss';
 
 type Inputs = {
   name: string;
@@ -24,7 +24,7 @@ interface Props {
 const QuoteForm: React.FC<Props> = ({ stretch }) => {
   const { register, handleSubmit, errors } = useForm<Inputs>();
   const [showModal, setShowModal] = useState(false);
-  const [modalText, setModalText] = useState("");
+  const [modalText, setModalText] = useState('');
   const [loading, setLoading] = useState(false);
 
   const onSubmit = async (values: any, e: any) => {
@@ -38,20 +38,44 @@ const QuoteForm: React.FC<Props> = ({ stretch }) => {
 
   return (
     <SlipInWhenVisible>
-      <div className={classNames(styles.quoteForm, { [styles.quoteFormStretch]: stretch })}>
-        <Modal showModal={showModal} setShowModal={setShowModal} modalText={modalText} />
+      <div
+        className={classNames(styles.quoteForm, {
+          [styles.quoteFormStretch]: stretch,
+        })}
+      >
+        <Modal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          modalText={modalText}
+        />
         <div className={styles.info}>
           <h2 className={styles.heading}>Our Location</h2>
           <p className={styles.content}>
-            Plot No.ISP -10, Opposite Sanghi Organization, Near Petrol Pump, MIDC Taloja Industrial Area, Navi Mumbai, 410208, Maharashtra, India
+            Plot No.ISP -10, Opposite Sanghi Organization, Near Petrol Pump,
+            MIDC Taloja Industrial Area, Navi Mumbai, 410208, Maharashtra, India
           </p>
           <h2 className={styles.heading}>Our Email ID</h2>
           <p className={styles.content}>safelineelectricals@gmail.com</p>
 
-          <h2 className={styles.call}>We will get back to you within 24 hours, or call us everyday, 10:00 AM - 06:00 PM</h2>
-          <p className={styles.heading2}>
-            <Telephone /> +91-8425846364
-          </p>
+          <h2 className={styles.call}>
+            We will get back to you within 24 hours, or call us everyday, 10:00
+            AM - 06:00 PM
+          </h2>
+          <div style={{ display: 'flex', alignItems: 'flex-end', gap: '2rem' }}>
+            <p className={styles.heading2}>
+              <Telephone />
+            </p>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '8px',
+              }}
+            >
+              <p style={{ color: 'white' }}> +91-9372156520</p>
+              <p style={{ color: 'white' }}> +91-8104861607</p>
+            </div>
+          </div>
         </div>
         <div className={styles.form}>
           <div className={styles.title}>
@@ -60,15 +84,19 @@ const QuoteForm: React.FC<Props> = ({ stretch }) => {
           </div>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className={styles.first}>
-              <input name="name" placeholder="Your Full Name" ref={register({ required: "Your name is required" })} />
+              <input
+                name="name"
+                placeholder="Your Full Name"
+                ref={register({ required: 'Your name is required' })}
+              />
               <input
                 name="email"
                 placeholder="Your Email ID"
                 ref={register({
-                  required: "your email id is required.",
+                  required: 'your email id is required.',
                   pattern: {
                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: "Invalid Email",
+                    message: 'Invalid Email',
                   },
                 })}
               />
@@ -78,33 +106,84 @@ const QuoteForm: React.FC<Props> = ({ stretch }) => {
                 name="phone"
                 placeholder="Your Phone Number"
                 ref={register({
-                  required: "Your phone number is required.",
+                  required: 'Your phone number is required.',
                   maxLength: {
                     value: 10,
-                    message: "This input exceed 10 digits.",
+                    message: 'This input exceed 10 digits.',
                   },
                   minLength: {
                     value: 10,
-                    message: "Your number should be of 10 digits",
+                    message: 'Your number should be of 10 digits',
                   },
                 })}
               />
 
-              <input name="zip" placeholder="Your Zip Code" ref={register({ required: "Your zip code is required" })} />
+              <input
+                name="zip"
+                placeholder="Your Zip Code"
+                ref={register({ required: 'Your zip code is required' })}
+              />
             </div>
             <div className={styles.third}>
-              <input name="service" placeholder="Name of the service that you want" ref={register({ required: "Please enter which service you want." })} />
+              <input
+                name="service"
+                placeholder="Name of the service that you want"
+                ref={register({
+                  required: 'Please enter which service you want.',
+                })}
+              />
             </div>
             <div className={styles.fourth}>
-              <textarea name="message" placeholder="Tell us more about your requirements" ref={register({ required: false })} rows={10} />
+              <textarea
+                name="message"
+                placeholder="Tell us more about your requirements"
+                ref={register({ required: false })}
+                rows={10}
+              />
             </div>
             <div className={styles.errors}>
-              <ErrorMessage errors={errors} name="name" render={({ message }) => <p className={styles.error}>{message}</p>} />
-              <ErrorMessage errors={errors} name="phone" render={({ message }) => <p className={styles.error}>{message}</p>} />
-              <ErrorMessage errors={errors} name="email" render={({ message }) => <p className={styles.error}>{message}</p>} />
-              <ErrorMessage errors={errors} name="zip" render={({ message }) => <p className={styles.error}>{message}</p>} />
-              <ErrorMessage errors={errors} name="service" render={({ message }) => <p className={styles.error}>{message}</p>} />
-              <ErrorMessage errors={errors} name="message" render={({ message }) => <p className={styles.error}>{message}</p>} />
+              <ErrorMessage
+                errors={errors}
+                name="name"
+                render={({ message }) => (
+                  <p className={styles.error}>{message}</p>
+                )}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="phone"
+                render={({ message }) => (
+                  <p className={styles.error}>{message}</p>
+                )}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="email"
+                render={({ message }) => (
+                  <p className={styles.error}>{message}</p>
+                )}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="zip"
+                render={({ message }) => (
+                  <p className={styles.error}>{message}</p>
+                )}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="service"
+                render={({ message }) => (
+                  <p className={styles.error}>{message}</p>
+                )}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="message"
+                render={({ message }) => (
+                  <p className={styles.error}>{message}</p>
+                )}
+              />
             </div>
             <button type="submit" disabled={loading}>
               Submit <Arrow />
--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,23 +1,100 @@
-import { PageProps } from "gatsby";
-import React from "react";
-import CVForm from "../components/CVForm/CVForm";
-import Layout from "../components/Layout/Layout";
-import SEO from "../components/SEO/SEO";
-import styles from "../styles/index.module.scss";
+import { PageProps } from 'gatsby';
+import React from 'react';
+import CVForm from '../components/CVForm/CVForm';
+import Layout from '../components/Layout/Layout';
+import SEO from '../components/SEO/SEO';
+import styles from '../styles/index.module.scss';
 
 interface Props {}
 const careers: React.FC<PageProps> = ({ path }) => {
   return (
     <Layout path={path}>
       <div className={styles.careers}>
-        <SEO title={"Join Us"} description={null} />
+        <SEO title={'Join Us'} description={null} />
         <div className={styles.text}>
           <h1 className={styles.heading}>
             <span>Join Us</span>
           </h1>
-          <p>Diploma in Electrical Engineering 1-2 years</p>
-          <p>Experience Electrician ITI with NCTVT 1-5 years</p>
-          <p>Experience Wireman PWD license Holder 2-3 years Experience</p>
+          <div style={{ marginBottom: '2rem' }}>
+            <h3 style={{ margin: '8px 0' }}>PROJECT MANAGER</h3>
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Education
+              </span>
+              : Having degree in Civil, Mechanical & Realated Civil Construction
+              Work.
+            </div>
+
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Experience
+              </span>
+              : 10 to 12 year in Handling Project of Civil Construction Work.
+            </div>
+
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Qualities
+              </span>
+              : Arrangement of Manpower and Good Communication Skill with Team
+              ,problem solving capacity and good leadership, Negociation with
+              sub contractor and cost manegement , Risk Management Skill and
+              Critical Thinking.
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '2rem' }}>
+            <h3 style={{ margin: '8px 0' }}>SITE SUPERVISIOR</h3>
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Education
+              </span>
+              : Having Degree/Diploma in Civil , Mechanical & Equivalant
+              Qualification.
+            </div>
+
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Experience
+              </span>
+              : 5 to 7 Year in handling Project of Civil Construction Work.
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '2rem' }}>
+            <h3 style={{ margin: '8px 0' }}>ELECTRICIAN & WIREMAN</h3>
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Education
+              </span>
+              : : ITI 2 years course completed with NCTVT with Wireman License
+              Compulsary.
+            </div>
+
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Experience
+              </span>
+              : : 2 to 5 Years in industrial Project and Maintainance.
+            </div>
+          </div>
+
+          <div style={{ marginBottom: '2rem' }}>
+            <h3 style={{ margin: '8px 0' }}>FITTER/WELDER</h3>
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Education
+              </span>
+              : : ITI 2 years course completed with NCTVT
+            </div>
+
+            <div style={{ margin: '12px 0' }}>
+              <span style={{ fontWeight: 'bold', display: 'inline' }}>
+                Experience
+              </span>
+              : : 2 to 5 Years exprience in pipeline and structure work.
+            </div>
+          </div>
         </div>
         <div className={styles.form}>
           <CVForm />
